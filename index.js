5247be075f5ba027a4ab4c6a8ab374468162045e
xiaohui <xiaohui@XIAO-PC>
2024-05-12 00:27:09 +0200

    hente data fra rest

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() {
-    function updateHeartRate() {
-        document.getElementById('staticBPM').textContent = `${Math.floor(Math.random() * 15 + 60)} BPM`; 
-        document.getElementById('activeBPM').textContent = `${Math.floor(Math.random() * 20 + 100)} BPM`; 
-        document.getElementById('AfterExerciseBPM').textContent = `${Math.floor(Math.random() * 10 + 80)} BPM`; 
-        document.getElementById('StressBPM').textContent = `${Math.floor(Math.random() * 10 + 80)} BPM`;
+const baseUrl = "https://pulsemaalerrestapi.azurewebsites.net/api/pulses"
+Vue.createApp({
+    data() {
+        return {
+            allPersons: [],
+         persons:[],
+         person:[],
+        name: null
+        
+        }
+    },
+    async created() { // life cycle method. Called when browser reloads page
+        this.getAll(baseUrl)
+    },
+    methods: {
+        async getAll(baseUrl) {
+            try {
+                const response = await axios.get(baseUrl)
+                this.allPersons = await response.data
+                this.persons = this.allPersons
+                console.log(this.allPersons)
+                console.log(this.persons)
+            } catch (ex) {
+                alert(ex.message) 
+            }
+        },
+        filterByName(name) {
+          
+            this.person = this.allPersons.filter(b => b.name.includes(name))
+            console.log(this.person)
+        }
+
     }
 
-    setInterval(updateHeartRate, 1000); 
-});
+}).mount("#app")
